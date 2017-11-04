b55d33342e9d5713ab85fe23eee9a6599ea091e7
molon <dudl@qq.com>
2017-11-05 00:48:24 +0800

    bak

--- main.go
+++ main.go
@@ -2,34 +2,8 @@ package main
 
 import (
 	"fmt"
-	"runtime"
 )
 
 func main() {
-	hello()
-}
-
-func hello() {
-	pc, file, line, ok := runtime.Caller(0)
-	if !ok {
-		return
-	}
-
-	// get Fun object from pc
-	fun := runtime.FuncForPC(pc)
-	funcNmae := fun.Name()
-
-	fmt.Printf("[%s:%d] Func name %s\n", file, line, funcNmae)
-
-	pc, file, line, ok = runtime.Caller(100)
-	if !ok {
-		return
-	}
-
-	// get Fun object from pc
-	fun = runtime.FuncForPC(pc)
-	funcNmae = fun.Name()
-
-	fmt.Printf("[%s:%d] Func name %s\n", file, line, funcNmae)
-
+	fmt.Println("Please see the main_test.go")
 }
